7bf6ce5951e7c8f9d1205f3cc1749f76df896abe
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-17 13:48:48 +0700

    Create `firebase.js`

---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// src/index.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7VEjBOSp1WofZY96Rb_JlpD7yePqODsM",
+  authDomain: "basic-app-6d38b.firebaseapp.com",
+  projectId: "basic-app-6d38b",
+  storageBucket: "basic-app-6d38b.appspot.com",
+  messagingSenderId: "495084675859",
+  appId: "1:495084675859:web:217c9c99caf7ed8f31f0e6",
+  measurementId: "G-J79N2E3267"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export {
+  firebaseConfig,
+  firebaseApp,
+  db
+};
