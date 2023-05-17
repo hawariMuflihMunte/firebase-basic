cc1266fddb968382fdfedae847e0a6c1dc66f986
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-17 13:51:29 +0700

    Import `firebase/auth`

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,9 +19,11 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp)
 
 export {
   firebaseConfig,
   firebaseApp,
-  db
+  db,
+  auth
 };
